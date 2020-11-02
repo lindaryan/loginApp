fae693fb7c2e157fe951fbc32d68280350512171
Linda Ryan <linda.ryan@mygeorgian.ca>
2020-11-02 13:16:14 -0500

    Initial Commit

---
+++ loginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginScreen
+//
+//  Created by Linda 👱🏻‍♀️ on 2020-11-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
